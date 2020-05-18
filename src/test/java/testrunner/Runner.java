f66fd4cedf7ff0e2d0d5322fbb1103c66ad880db
Balapavan <balapavan123@gmail.com>
2020-05-18 14:25:44 +0530

    Added Comments and tag details

--- src/test/java/testrunner/Runner.java
+++ src/test/java/testrunner/Runner.java
@@ -8,14 +8,27 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions
 		(
-				features=".//Features/CreateUser.feature",
-				glue="stepDefnetions",
-				dryRun=false,
-				monochrome=true,
-				plugin= {"pretty","html:test-outputfolder"},
-				tags= {"Sanity"}
+				features=".//Features/", //--> Declare feature file path
+				glue="stepDefnetions",   //--> Step definition package name
+				dryRun=true,             //--> To check all Steps are implemented or not Note: Scripts won't execute
+				monochrome=true,         //--> Output will come Readable format 
+				plugin= {"pretty","html:test-outputfolder"}, 
+				tags= {"@Smoke"},         //--> Run based on Tag name
+				strict=true				  //--> Scipts will execute even-though steps are not implemented and it throw run time Exception    	
 		)
-
+/*** Tags Declarations ****--> Tags are used to specify which scenarios need to be Run 
+ **** Run based on Single tag name ***
+ 		* tags= {"@Smoke"} 
+ **** And condition declarations ****
+ 		* tags= {“@Sanity”, “@Smoke”} 
+ 		* tags= {"@Sanity and @Smoke"}
+ 								
+ **** OR condition Declarations
+ 		* tags= {"@Sanity or @Smoke"} 
+ **** And and or Conditions ****
+ 		* tags= {"@Smoke", "@Sanity or @Regression"} --> Along with Smoke tag either Sanity or regression should be there 
+ 		 													then only those Scenarios will execute  
+ */
 public class Runner {
 
 }
